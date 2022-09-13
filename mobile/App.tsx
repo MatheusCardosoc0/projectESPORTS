4a7634464a2596cf75f2deab8316a12b8f147e5a
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2022-09-13 20:57:01 -0300

    estilizando versão mobile da aplicação

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,39 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { View } from 'react-native';
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>eae</Text>
-
-      <Button title='eae'></Button>
-      <Button title='Clique me'></Button>
-
-      <StatusBar style="auto" backgroundColor='teal' />
-    </View>
+    <Background>
+      
+    </Background>
   );
 }
 
-interface ButtonProps{
-  title: string
-}
 
-function Button(props: ButtonProps){
-  return(
-  <TouchableOpacity>
-    <Text>{props.title}</Text>
-  </TouchableOpacity>)
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title:{
-    color: 'teal',
-    fontSize: 30
-  }
-});
