ed9dc6dea2193432048db865024bfba3061f1b25
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2022-09-14 18:14:31 -0300

    configurando banco de dados com prisma e msqlite

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,26 @@ import  express  from "express";
 
 const app = express()
 
-app.get('/ads',(req, res) => {
+app.get('/games/:id/ads',(req, res) => {
+
+  const gamesId = req.params
+
+
   return res.json([
     {nome: 'bobo'},
     {nome: 'jaqueline'},
     {nome: 'dedei'},
     {nome: 'marr'}
+  ])
+})
+
+app.get('/ads/:id/discord',(req, res) => {
+
+  const adId = req.params
+
+
+  return res.json([
+    
   ])
 })
 
