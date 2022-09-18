21f7ec511a46b5760b17f748fb564f020b12679d
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2022-09-18 09:05:35 -0300

    refazendo backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,102 +1,10 @@
-import  express  from "express";
-import cors from 'cors'
-import { PrismaClient } from "@prisma/client";
+import express, { application } from 'express'
+
 
 const app = express()
-const prisma = new PrismaClient()
 
-app.use(express.json())
-app.use(cors())
-
-
-
-app.get('/games', async (req, res) =>{
-  const games = await prisma.game.findMany({
-    include:{
-      _count:{
-        select:{
-          Ads: true
-        }
-      }
-    }
-  })
-
-  return res.json(games)
-})
-
-
-app.post('/games/:id/ads',async (req, res) =>{
-
-  const gameId = req.params.id
-  const body: any = req.body
-
-  const ad = await prisma.ad.create({
-    data:{
-      gameId,
-      name: body.name,
-      yearsPlaying: body.yearsPlaying,
-      discord: body.discord,
-      weekDays: body.weekDays.join(','),
-      hourStart: body.hourStart,
-      hoursEnd: body.hoursEnd,
-      useVoiceChannel: body.useVoiceChannel,
-      id: body.name
-    }
-  })
-
-
-  return res.status(201).json(ad)
-})
-
-
-app.get('/games/:id/ads',async (req, res) => {
-
-  const gamesId = req.params.id
-
-  const ads = await prisma.ad.findMany({
-    select:{
-      name: true,
-      id: true,
-      weekDays: true,
-      hourStart: true,
-      hoursEnd: true,
-      yearsPlaying: true,
-      useVoiceChannel: true
-    },
-    where:{
-      gameId: gamesId
-    },
-    orderBy:{
-      createdAt: 'desc'
-    }
-  })
-
-
-  return res.json(ads.map( ad => {
-    return {
-      ...ad,
-      weekDays: ad.weekDays.split(',')
-    }
-  }))
-})
-
-app.get('/ads/:id/discord',async(req, res) => {
-
-  const adId = req.params.id
-
-  const ad = await prisma.ad.findUniqueOrThrow({
-    select: {
-      discord: true,
-    },
-    where:{
-      id: adId
-    }
-  })
-
-
-  return res.json({
-    discord: ad.discord
-  })
+app.get('/ads', (require, response) => {
+  response.send('eae')
 })
 
 app.listen(3777)
